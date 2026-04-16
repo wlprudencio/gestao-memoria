f48f4e32727253eb0251334f64054d4adbbec5ca
WallacePrudencio <119940315+wlprudencio@users.noreply.github.com>
2026-04-16 17:25:57 -0300

    Primeira subida gestao-memoria

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class AgendaDeConsultas {
+public class  AgendaDeConsultas {
 
     @Autowired
     private ConsultaRepository consultaRepository;
